b9513e135f12fc0a41334e2bf91f98969f2771f9
RowinVA <rowin.amsterdam@gmail.com>
2022-06-16 22:19:23 +0200

    Added link to issue one-dark theme

--- components/utils/RichContent/patch/observable.d.ts
+++ components/utils/RichContent/patch/observable.d.ts
@@ -1,5 +1,6 @@
 // observable.d.ts
 // Declare Prism oneDark theme missing from @types/react-syntax-highlighter
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/439
 
 declare module 'react-syntax-highlighter/dist/cjs/styles/prism' {
     export { default as oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism/one-dark'
