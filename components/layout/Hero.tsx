88ede64a031d363f9308c98ae1186564bfdf6580
RowinVA <rowin.amsterdam@gmail.com>
2022-06-22 16:54:38 +0200

    Optimized stars banner and added container to align center

--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -1,13 +1,19 @@
 export const Hero = () => {
     return (
         <section className="hero">
-            <div id="stars"></div>
-            <div id="stars2"></div>
-            <div id="stars3"></div>
+            <div className="stars-container">
+                <div id="stars" />
+                <div id="stars2" />
+                <div id="stars3" />
+            </div>
 
             <div id="title" className="hero__title-container container">
-                <h2 className="hero__subtitle"><span>Web2 • Web3</span></h2>
-                <h1 className="hero__title"><span>Sharing my code-related learnings</span></h1>
+                <h2 className="hero__subtitle">
+                    <span>Web2 • Web3</span>
+                </h2>
+                <h1 className="hero__title">
+                    <span>Sharing my code-related learnings</span>
+                </h1>
             </div>
         </section>
     );
